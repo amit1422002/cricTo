932a3dcd3961abf3834d38c783d2b3ea105e6317
Amit Singh <Amit@6720-CA48.9431>
2024-06-26 20:43:11 +0530

    api made for live matches

---
+++ cricToBackend/src/main/java/com/crick/apis/services/MatchService.java
@@ -0,0 +1,20 @@
+package com.crick.apis.services;
+
+import com.crick.apis.entities.Match;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MatchService {
+
+
+    List<Match> getAllMatches();
+
+// get live matches
+
+
+    List<Match> getLiveMatches();
+    // get cwc2023 point table Endpoints
+
+    List<Map<String, String>> getPointTable();
+}
